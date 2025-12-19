970d228307917e5835772cc1f9ca0bb42bcac285
312308239 Rafat Reda Jahin Ahmed Jahin <rafat312308239@med.helwan.edu.eg>
2025-12-19 09:48:15 +0200

    Updating Excalidraw objectives

--- src/components/Client/Products/ProductPage/ProductPage.jsx
+++ src/components/Client/Products/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ProductPage = () => {
+    // what we need now is to render the product page accroding to the product id from the url
   return (
     <div>ProductPage</div>
   )
