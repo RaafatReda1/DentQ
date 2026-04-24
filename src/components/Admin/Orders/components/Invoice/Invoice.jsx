a676349848ff3ba9f894c4fe1d3b450f8179269d
312308239 Rafat Reda Jahin Ahmed Jahin <rafat312308239@med.helwan.edu.eg>
2026-04-24 21:04:08 +0300

    Invoice ProductsTable Header is done and the lap is about to die so iam saving the progress

--- src/components/Admin/Orders/components/Invoice/Invoice.jsx
+++ src/components/Admin/Orders/components/Invoice/Invoice.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles  from "./Invoice.module.css";
 import InvoiceHeader from "./InvoiceHeader/InvoiceHeader";
 import RecepientData from "./RecepientData/RecepientData";
+import OrderDetails from "./OrderDetails/OrderDetails";
 const Invoice = React.forwardRef(({ order }, ref) => {
   return (
     <div ref={ref} className={styles.Invoice}>
       <InvoiceHeader />
       <RecepientData />
+      <OrderDetails/>
     </div>
   );
 });
---
+++ src/components/Admin/Orders/components/Invoice/OrderDetails/OrderDetails.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./OrderDetails.module.css";
+const OrderDetails = () => {
+  return (
+    <section className={styles.orderDetailsSection}>
+      <table className={styles.productsTable}>
+        <thead>
+          <th>Qty</th>
+          <th>Product</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Total</th>
+        </thead>
+
+        <tbody>
+            <td>3</td>
+            <td>Iphone 15 Pro Max</td>
+            <td>This is an extremely valuable iphone that can call anyone everywhere...</td>
+            <td>120</td>
+            <td>360</td>
+
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default OrderDetails;
