f9a3bbd98e18748c4ba7cc5f62e876c1b2350832
312308239 Rafat Reda Jahin Ahmed Jahin <rafat312308239@med.helwan.edu.eg>
2026-04-29 11:11:40 +0300

    Adding the Loader

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,33 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import './utils/i18n.js';
+import Loader from './components/Shared/Loader/Loader.jsx';
+import { useEffect } from 'react';
+
 
 export function Root() {
   const [appKey, setAppKey] = useState(0);
   const [isFadingOut, setIsFadingOut] = useState(false);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsInitialLoading(false);
+    }, 8500);
+    return () => clearTimeout(timer);
+  }, []);
 
   window.forceRerender = () => {
     setIsFadingOut(true);
     setTimeout(() => {
       setAppKey((k) => k + 1);
       setIsFadingOut(false);
-    }, 500); // Wait for transition
+    }, 500);
   };
+
+  if (isInitialLoading) {
+    return <Loader theme="light" />;
+  }
 
   return (
     <StrictMode>
